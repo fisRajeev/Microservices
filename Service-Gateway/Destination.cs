5c88409d9d04252ee8bcbaf3b1ff75f4655fc2c1
fisRajeev <41896099+fisRajeev@users.noreply.github.com>
2020-08-31 18:01:07 +0530

    Add files via upload

---
+++ Service-Gateway/Destination.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OzNet
+{
+    public class Destination
+    {
+        public string Path { get; set; }
+        public bool RequiresAuthentication { get; set; }
+        static HttpClientHandler handler = new HttpClientHandler();
+        static HttpClient client = new HttpClient(handler, false);
+        public Destination(string uri, bool requiresAuthentication)
+        {
+            Path = uri;
+            RequiresAuthentication = requiresAuthentication;
+        }
+
+        public Destination(string path)
+            :this(path, false)
+        {
+        }
+
+        private Destination()
+        {
+            Path = "/";
+            RequiresAuthentication = false;
+        }
+
+        public async Task<HttpResponseMessage> SendRequest(HttpRequest request)
+        {
+            string requestContent;
+            using (Stream receiveStream = request.Body)
+            {
+                using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    requestContent = readStream.ReadToEnd();
+                }
+            }
+
+            using (var newRequest = new HttpRequestMessage(new HttpMethod(request.Method), CreateDestinationUri(request)))
+            {
+                newRequest.Content = new StringContent(requestContent, Encoding.UTF8, request.ContentType);
+                handler.Credentials = CredentialCache.DefaultCredentials;
+                client = new HttpClient(handler, false);
+                var response = await client.SendAsync(newRequest);
+             
+                return response;              
+            }
+        }
+
+        private string CreateDestinationUri(HttpRequest request)
+        {
+            string requestPath = request.Path.ToString();
+            string queryString = request.QueryString.ToString();
+            string endpoint = string.Empty;
+
+            if (string.IsNullOrEmpty(requestPath) == false && requestPath.Length > 1)
+            {
+                if (requestPath.Substring(1).IndexOf('/') > 0)
+                {
+                    endpoint = requestPath.Substring(requestPath.Substring(1).IndexOf('/') + 2);
+                }
+            }
+
+            return Path + (string.IsNullOrEmpty(endpoint) == false ? "/" + endpoint : string.Empty) 
+                + (string.IsNullOrEmpty(queryString) == false ? "/" + queryString : string.Empty);
+        }
+
+    }
+}
---
+++ Service-Gateway/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using OzNet;
+
+namespace Example
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+       
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            HttpResponseMessage content;
+            Router router = new Router("routes.json");
+            app.Run(async (context) =>
+            {
+                content = await router.RouteRequest(context.Request);
+                string result = await content.Content.ReadAsStringAsync();
+                               
+               await context.Response.WriteAsync(result);                
+            });
+
+           
+
+            //app.Run(context =>
+            //{
+            //    context.Response.OnStarting(async () =>
+            //    {
+            //        //content = await router.RouteRequest(context.Request);
+            //        content = await router.RouteRequest(context.Request);
+            //        await context.Response.WriteAsync(await content.Content.ReadAsStringAsync());
+            //    });
+            //    return Task.CompletedTask;
+            //});
+        }
+    }
+}
